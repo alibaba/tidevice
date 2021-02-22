fc5ac0ed83feee2d626da50ede3c89eecc61e4e3
codeskyblue <codeskyblue@gmail.com>
2021-02-22 15:53:08 +0800

    limit python version >= 3.7

--- setup.py
+++ setup.py
@@ -7,4 +7,4 @@ if sys.version_info[:2] <= (3, 6):
     sys.exit("\n*** tidevice requires Python version 3.7+")
 
 setuptools.setup(
-    setup_requires=['pbr'], pbr=True, python_requires=">=3.6")
+    setup_requires=['pbr'], pbr=True, python_requires=">=3.7")
