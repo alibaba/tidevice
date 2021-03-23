2020fb5348f0fc5ff417a6a7684ae2be0a9ca6a4
codeskyblue <codeskyblue@gmail.com>
2021-03-23 16:38:07 +0800

    fix typo, close #42

--- tidevice/_device.py
+++ tidevice/_device.py
@@ -191,7 +191,7 @@ class BaseDevice():
             from ._ssl import make_certs_and_key
         except ImportError:
             print("DevicePair require pyOpenSSL and pyans1, install by the following command")
-            print("\tpip3 install pyOpenSSL pyans1", flush=True)
+            print("\tpip3 install pyOpenSSL pyasn1", flush=True)
             raise RuntimeError("Missing lib")
 
         cert_pem, priv_key_pem, dev_cert_pem = make_certs_and_key(device_public_key)
