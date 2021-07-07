5a179c0d6427104d4a538a2eec6f3c0b54f4d77c
codeskyblue <codeskyblue@gmail.com>
2021-07-07 14:38:58 +0800

    fix 14.6 image missing bug, not tested

--- tidevice/_imagemounter.py
+++ tidevice/_imagemounter.py
@@ -44,6 +44,7 @@ def get_developer_image_url_list(version: str) -> typing.List[str]:
     github_repo = "JinjunHan/iOSDeviceSupport"
     _alias = {
         "12.5": "12.4.zip"
+        "14.6": "14.5.zip"
     }
     zip_name = _alias.get(version, f"{version}.zip")
 
