83821e981744fce6f38a449f672c6f5b163ec16b
codeskyblue <codeskyblue@gmail.com>
2022-03-24 20:05:15 +0800

    comment set_ciphers

--- tidevice/_safe_socket.py
+++ tidevice/_safe_socket.py
@@ -98,7 +98,11 @@ class SafeStreamSocket:
         self._dup_sock = self._sock.dup()
         
         context = ssl.SSLContext()
-        context.set_ciphers("DEFAULT:@SECLEVEL=0") # fix md_too_weak error
+        # try:
+        #     context.set_ciphers("DEFAULT:@SECLEVEL=0") # fix md_too_weak error
+        # except ssl.SSLError:
+        #     # no ciphers can be selected.
+        #     pass
         context.load_cert_chain(pemfile, keyfile=pemfile)
         context.check_hostname = False
         ssock = context.wrap_socket(self._sock, server_hostname="iphone.localhost")
