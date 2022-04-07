bb0c56eb05bf10fbd48c3f9dd0f811d3e7192306
codeskyblue <codeskyblue@gmail.com>
2022-04-07 11:24:21 +0800

    update run-usbmux-proxy.sh

--- scripts/run-usbmuxd-proxy.sh
+++ scripts/run-usbmuxd-proxy.sh
@@ -3,14 +3,17 @@
 # run command first: sudo mv /var/run/usbmuxd /var/run/usbmuxx
 # forward through socat: sudo socat -t100 -x -v UNIX-LISTEN:/var/run/usbmuxd,mode=777,reuseaddr,fork UNIX-CONNECT:/var/run/usbmuxx
 
+PROGRAM="$(basename $0)"
 SOCKET="/var/run/usbmuxd"
 BACKUP_SOCKET="$(dirname "${SOCKET}")/$(basename "${SOCKET}").tidevice_bak"
 FORWARD_SOCKET=${BACKUP_SOCKET}
+CURDIR=$(cd $(dirname $0); pwd)
 
-if test $(whoami) != "root"; then
-	echo "Must be run as root"
-	exit 1
-fi
+USAGE="Usage:
+	$PROGRAM --help               Show this message
+	$PROGRAM [--ssl|-s]           Enable SSL inspect
+	$PROGRAM --tcp_redirect 6789
+"
 
 while [[ $# -gt 0 ]]; do
 	case $1 in
@@ -24,6 +27,10 @@ while [[ $# -gt 0 ]]; do
 		shift # past argument
 		shift # past value
 		;;
+	-h | --help)
+		echo -n "$USAGE"
+		exit 0
+		;;
 	-* | --*)
 		echo "Unknown option $1"
 		exit 1
@@ -34,10 +41,29 @@ while [[ $# -gt 0 ]]; do
 	esac
 done
 
-DEVICES=($(tidevice list | sed -n '2p'))
-UDID=${DEVICES[0]}
-NAME=${DEVICES[1]}
-echo "UDID: ${UDID:?} ${NAME}"
+function safe_run(){
+	local DEBUG_COLOR="\033[0;32m"
+	local RESET="\033[0m"
+	echo -e "${DEBUG_COLOR}>> $@""${RESET}"
+	"$@"
+}
+
+if test $(whoami) != "root"; then
+	echo "Must be run as root"
+	exit 1
+fi
+
+if test $(tidevice list -1 | wc -l) != 1
+then
+	tidevice list
+	echo "-------------"
+	echo "ERROR: should connect one device"
+	exit 1
+fi
+
+UDID=$(tidevice list -1)
+tidevice version
+echo "UDID: ${UDID:?}"
 
 # 保存设备配对公钥
 tidevice savesslfile
@@ -52,12 +78,10 @@ fi
 
 function recover() {
 	echo "Recover"
-	mv ${BACKUP_SOCKET} ${SOCKET}
-	# kill socat
-	kill %%
+	safe_run mv ${BACKUP_SOCKET} ${SOCKET}
 }
 
-mv ${SOCKET} ${BACKUP_SOCKET}
+safe_run mv ${SOCKET} ${BACKUP_SOCKET}
 trap recover EXIT
 
 # 启用 tcp redirect 后可以用 https://github.com/douniwan5788/usbmuxd_debug.git 在wireshark中抓包分析
@@ -67,11 +91,11 @@ trap recover EXIT
 # sudo SSLKEYLOGFILE=./tlskeys.log ./run-usbmuxd-proxy.sh --ssl -t 9876
 if [ x"$REDIRECT_PORT" != x"" ]; then
 	# Setup pipe over TCP that we can tap into
-	socat -t100 "TCP-LISTEN:${REDIRECT_PORT},bind=127.0.0.1,reuseaddr,fork" "UNIX-CONNECT:${BACKUP_SOCKET}" &
-	echo $SSLKEYLOGFILE
+	safe_run socat -t100 "TCP-LISTEN:${REDIRECT_PORT},bind=127.0.0.1,reuseaddr,fork" "UNIX-CONNECT:${BACKUP_SOCKET}" &
+	echo "SSLKEYLOGFILE: $SSLKEYLOGFILE"
 	export SSLKEYLOGFILE
 fi
 
 # sudo socat -t100 -v UNIX-LISTEN:${SOCKET},mode=777,reuseaddr,fork UNIX-CONNECT:${SOCKET}.tidevice_orig
-python3 plistdump-tcp-proxy.py -L ${SOCKET} -F ${FORWARD_SOCKET} ${SSL} \
+python3 "$CURDIR/plistdump-tcp-proxy.py" -L ${SOCKET} -F ${FORWARD_SOCKET} ${SSL} \
 	--pemfile ${PEMFILE} "$@"
