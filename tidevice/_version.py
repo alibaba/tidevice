1d7bf00ab2e7946354fedaf9cd0eafc00c5a277e
steelonion <steelonion@foxmail.com>
2025-08-18 22:43:16 +0800

    Update _version.py to fix pkg_resources warning

--- tidevice/_version.py
+++ tidevice/_version.py
@@ -1,9 +1,16 @@
 # coding: utf-8
 # codeskyblue 2020/06/10
 
-import pkg_resources
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    import pkg_resources
+    def version(name):
+        return pkg_resources.get_distribution(name).version
+    PackageNotFoundError = pkg_resources.DistributionNotFound
+
 from ._proto import PROGRAM_NAME
 try:
-    __version__ = pkg_resources.get_distribution(PROGRAM_NAME).version
-except pkg_resources.DistributionNotFound:
+    __version__ = version(PROGRAM_NAME)
+except PackageNotFoundError:
     __version__ = "unknown"
